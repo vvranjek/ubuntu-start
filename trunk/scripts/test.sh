c7cd4adf4bc63a7f0aa9ef56eba26acd16800a93
Vid Vranjek <vidvranjek@gmail.com>
2015-02-22 22:36:45 +0100

    test

--- trunk/scripts/test.sh
+++ trunk/scripts/test.sh
@@ -1,13 +1,25 @@
-#!/bin/bash
+
+	while true; do
+	    read -p "Do you want to disable scrollbar overlay (Y/N)? To enable press E." answer
+	    case $answer in
+		[Yy]* ) if ! gsettings get com.canonical.desktop.interface scrollbar-mode | grep 'normal'
+			then
+				gsettings set com.canonical.desktop.interface scrollbar-mode normal
+				echo "Scrollbar overlay now disabled"
+			else
+				echo "Scrollbar allready disabled"
+			fi
+	 		break;;
+
+		[Nn]* ) echo "No"; break;;
+
+		[Ee]* ) gsettings set com.canonical.desktop.interface scrollbar-mode overlay-auto
+			echo "Scrollbar overlay enabled"
+			break;;
+
+		* ) echo "Please answer yes or no.";;
+	    esac
+	done
+	
 
 
-
-cur_dir=$(pwd)
-
-
-	#cp -ar "$cur_dir/../Themes/." "/home/$USER/.themes"
-	sudo bash -c "cp -ar $cur_dir/../Themes/. /root/.themes"
-
-
-
-#read -p "Press ENTER to exit..." answer
