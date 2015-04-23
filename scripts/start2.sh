6ec97f801ff1e38c231f1d75a85c0acb444a4605
Vid Vranjek <vidvranjek@gmail.com>
2015-04-23 13:11:45 +0200

    Installing ubuntu 15.04

--- scripts/start2.sh
+++ scripts/start2.sh
@@ -105,93 +105,18 @@ while true; do
     esac
 done
 
+FILENAME=package-list
 
 while true; do
     read -p "Do you want to install of apps (Y/N)?" answer
     case $answer in
       [Yy]* ) 
-
-      sudo apt-get update
-      sudo apt-get install icedtea-plugin -y
-      sudo apt-get install retext -y
-      sudo apt-get install grive-tools -y
-      sudo apt-get install ubuntu-tweak -y
-      sudo apt-get install google-chrome-stable -y
-      sudo apt-get install nautilus-dropbox -y
-      
-      sudo apt-get install terminator -y
-      sudo apt-get remove gnome-terminal
-      sudo ln -s /usr/bin/terminator /usr/bin/gnome-terminal
-      
-      sudo apt-get install jdownloader -y
-      sudo apt-get install ubuntu-restricted-extras -y
-      sudo apt-get install skype -y
-      sudo apt-get install compizconfig-settings-manager -y
-      sudo apt-get install compiz-plugins-extra -y
-      #sudo apt-get install nautilus-actions -y
-      #sudo apt-get remove  banshee -y
-      #sudo apt-get install rhythmbox -y
-      sudo apt-get install mountmanager -y
-      sudo apt-get install xoscope -y
-      sudo apt-get install wine -y
-      sudo apt-get install flashplugin-installer -y
-      sudo apt-get install vlc -y
-      sudo apt-get install unetbootin -y
-      sudo apt-get install fdupes -y
-      #sudo  apt-get install gnome-panel -y
-      sudo apt-get install tuxguitar timidity tuxguitar-jsa -y
-      #sudo  apt-get install drawers -y
-      sudo apt-get install dayfolder -y
-      #sudo  apt-get install myagenda -y
-      sudo apt-get install sl -y
-      
-      #nautilus
-
-      sudo apt-get install nautilus-actions-extra -y
-      sudo apt-get install nautilus-open-terminal -y
-      sudo apt-get install nautilus-image-converter -y
-      sudo apt-get install nautilus-actions -y
-      sudo apt-get install nautilus-scripts-manager -y
-      
-      sudo apt-get install rabbitvcs-cli -y rabbitvcs-core -y rabbitvcs-gedit -y rabbitvcs-nautilus3 -y
-
-      sudo apt-get install aptitude -y
-      sudo apt-get install git -y
-      sudo apt-get install qbittorrent -y
-      sudo apt-get install gconf-editor -y
-      sudo apt-get install startupmanager -y
-      sudo apt-get install dconf-tools -y
-      #sudo  apt-get install indicator-weather -y
-      sudo apt-get install indicator-multiload -y
-      sudo apt-get install compiz-fusion-plugins-apt -y
-      #sudo apt-get install gnome-pie -y
-      #sudo apt-get install myunity -y
-      sudo apt-get install gimp -y
-      sudo apt-get install clementine -y
-      sudo apt-get install calibre -y
-
-      sudo apt-get install build-essential -y
-      sudo apt-get install cmake -y
-      sudo apt-get install subversion -y
-      sudo apt-get install libgtk2.0-dev -y
-      sudo apt-get install pkg-config -y
-      sudo apt-get install libjpeg-dev -y
-      sudo apt-get install python-dev -y
-      sudo apt-get install yasm -y
-      sudo apt-get install openshot openshot-doc -y
-      sudo apt-get install minitube -y
-      sudo apt-get install pinta -y
-      sudo apt-get install picasa -y
-      sudo apt-get install dia -y
-      sudo apt-get install xsane -y
-      sudo apt-get install stellarium -y
-      sudo apt-get install xbmc -y
-      sudo apt-get install screenlets -y
-      sudo apt-get install screenlets-pack-all -y
-      sudo apt-get install kid3-qt -y
-      sudo apt-get install tagtool -y
-      sudo apt-get install gpointing-device-settings -y
-      sudo apt-get install solaar -y
+	
+	cat $FILENAME | while read -r line
+	do
+	      echo line is $line
+	done
+	
 
       #RUN PROGRAMS
       indicator-weather &
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,1 +1,32 @@
-echo TEEEEST
+
+FILENAME=package-list2
+
+ERROR_PKG="These are errors\n"
+ERROR_PKG2="TH\n"
+
+cat $FILENAME | while read -r line
+	do
+	      #sudo sh -c 'apt-get install $line'
+	      sudo apt-get install $line
+	      if [ $? != 0 ]; then
+		  echo "ERROR!!!!!!! in line $line"
+		  echo $line
+		  echo $line
+		  echo $line
+		  echo $line
+		  
+		  ERROR_PKG2+="$ERROR_PKG $line"
+		  ERROR_PKG="$ERROR_PKG $line"
+		  echo "$ERROR_PKG"
+		  echo "$ERROR_PKG2"
+	      fi
+		  
+		  
+	done
+	
+	echo "Final line is $line"
+
+	ERROR_PKG2+="$ERROR_PKG2 KVajzddeeeeeeeeeeeeeej"
+	ERROR_PKG2+="$ERROR_PKG $line"
+	echo "$ERROR_PKG"
+	echo "$ERROR_PKG2"
