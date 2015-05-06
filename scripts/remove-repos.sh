4e667e292813b6dc75c9e30537e952a9d549196b
Vid Vranjek <vidvranjek@gmail.com>
2015-05-06 14:12:01 +0200

    remove-repos test

--- scripts/remove-repos.sh
+++ scripts/remove-repos.sh
@@ -12,11 +12,17 @@ if [[ -n "$ppa" ]]
 then
 	echo Found unknown sources:
 	echo "$ppa"
+	echo "" >> /home/$USER/log/removed-repos.log
+	echo "$(date)" >> /home/$USER/log/removed-repos.log
+	echo "$ppa" >> /home/$USER/log/removed-repos.log
+	echo "Following sources were removed:" >> /home/$USER/log/removed-repos.log
 
 	echo "$ppa" | while read -r line
 	do
 		#echo "Reding line: $line"	
 		echo "$line" | grep ^W | awk '{split($line, a, "/"); print "ppa:"a[4]"/"a[5]}' | xargs sudo add-apt-repository --remove	
+		
+		echo "$line" >> /home/$USER/log/removed-repos.log
 	done
 else
 	echo "No unknown sources found."
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,32 +1,29 @@
 
 FILENAME=package-list2
 
-ERROR_PKG="These are errors\n"
-ERROR_PKG2="TH\n"
+ERROR_PKG=""
+#ERROR_PKG2="TH\n"
 
-cat $FILENAME | while read -r line
-	do
-	      #sudo sh -c 'apt-get install $line'
-	      sudo apt-get install $line
-	      if [ $? != 0 ]; then
-		  echo "ERROR!!!!!!! in line $line"
-		  echo $line
-		  echo $line
-		  echo $line
-		  echo $line
-		  
-		  ERROR_PKG2+="$ERROR_PKG $line"
-		  ERROR_PKG="$ERROR_PKG $line"
-		  echo "$ERROR_PKG"
-		  echo "$ERROR_PKG2"
-	      fi
-		  
-		  
-	done
+#sudo -u
+JOSKE="0"
+
+#cat $FILENAME |
+while read -r line
+    do
+        sh -c 'sudo apt-get install $line'
+        #sudo apt-get install $line
+        
+        APT_ERROR=$?
+        
+        if [ APT_ERROR != 0 ]; then
+            echo "ERROR in line $line"
+                        
+            ERROR_PKG="$ERROR_PKG"$'\n'$line
+           
+        fi
+    done < $FILENAME
 	
-	echo "Final line is $line"
 
-	ERROR_PKG2+="$ERROR_PKG2 KVajzddeeeeeeeeeeeeeej"
-	ERROR_PKG2+="$ERROR_PKG $line"
-	echo "$ERROR_PKG"
-	echo "$ERROR_PKG2"
+echo "The following programs did not install"$'\n' "$ERROR_PKG"
+#echo "$ERROR_PKG2"
+echo $JOSKE
