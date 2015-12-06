e4990a46348cb76724de852c76b924b24977ff79
Vid Vranjek <vidvranjek@gmail.com>
2015-12-06 20:27:58 +0100

    - Added to package-list 	guake 	prime-indicator (inscuding repo) - Added zsh installation to start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -25,8 +25,11 @@ cp -r $CURRENT_DIR/. /home/$USER/bin
 sudo chmod +x /home/$USER/bin/*
 sleep 0.3
 
-echo "Adding $USER to group dialout"
+echo "Adding $USER to group dialout and sudo"
 sudo adduser $USER dialout
+sudo adduser ulu dialout
+sudo adduser ulu sudo
+sudo adduser ulu vid
 sleep 0.3
 
 echo "Adding open as root menu to nautilus"
@@ -83,7 +86,8 @@ while true; do
     sudo add-apt-repository ppa:rabbitvcs/ppa -y    #SVN nautilus stuff
     sudo add-apt-repository ppa:flozz/flozz    -y     # nautilus-terminal
     sudo add-apt-repository ppa:noobslab/apps   -y    #open-as-administrator 
-    sudo add-apt-repository ppa:daniel.pavel/solaar -y #Sollar, tool for logitech unigying receicers  
+    sudo add-apt-repository ppa:daniel.pavel/solaar -y #Sollar, tool for logitech unigying receicers 
+    sudo add-apt-repository sudo ppa:nilarimogard/webupd8 -y #Prime indicator 
     sudo add-apt-repository ppa:appgrid/stable -y      #Appgrid
     #sudo add-apt-repository ppa:gloobus-dev/gloobus-preview -y
     #sudo apt-add-repository ppa:screenlets/ppa -y 		# Scrennlets
@@ -119,6 +123,8 @@ while true; do
 		if [ "$fs" != "#" ]; then
 			sudo apt-get install $line -y
 		fi
+			
+		indicator-multiload & disown
 	done
 	break;;
  		
@@ -126,6 +132,23 @@ while true; do
 	* ) echo "Please answer yes or no.";;
     esac
 done
+
+while true; do
+    read -p "Whould you like install ZSH and Oh-My-Zsh? (Y/N)" answer
+    case $answer in
+      [Yy]* ) 
+
+	sudo apt-get install zsh
+	wget --no-check-certificate http://install.ohmyz.sh -O - | sh
+	chsh -s /bin/zsh vid
+	chsh -s /bin/zsh ulu
+	break;;
+ 		
+    [Nn]* ) echo "NO"; break;;
+	* ) echo "Please answer yes or no.";;
+    esac
+done
+
 
 
 echo Done!
