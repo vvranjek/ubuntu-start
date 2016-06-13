1a0b9e645edb1f9f29692c02dae6fb6920a929f5
Vid Vranjek <vidvranjek@gmail.com>
2016-06-13 16:31:38 +0200

    Modifiead touchpad.sh

--- scripts/touchpad.sh
+++ scripts/touchpad.sh
@@ -5,7 +5,7 @@ touchpad="ETPS/2 Elantech Touchpad"
 echo "Setting touchpad settings for $touchpad."
 
 # Synaptics Noise Cancelation
-#xinput set-prop "$touchpad" "Synaptics Noise Cancellation" 4, 4 
+xinput set-prop "$touchpad" "Synaptics Noise Cancellation" 0, 0
 
 # Pointer speed and acceleration
 #xinput set-prop "$touchpad" "Device Accel Velocity Scaling" 1
@@ -21,8 +21,8 @@ echo "Setting touchpad settings for $touchpad."
 #xinput set-prop "$touchpad" "Synaptics Two-Finger Scrolling" 1, 0
 
 # Enable palm detection and set reseanoble propeties
-xinput set-prop "$touchpad" "Synaptics Palm Detection" 1
-xinput set-prop "$touchpad" "Synaptics Palm Dimensions" 5, 10
+#xinput set-prop "$touchpad" "Synaptics Palm Detection" 1
+#xinput set-prop "$touchpad" "Synaptics Palm Dimensions" 5, 20
 
 #xinput set-prop "$touchpad" "Synaptics Locked Drags" 1
 #xinput set-prop "$touchpad" "Synaptics Locked Drags Timeout" 500
@@ -34,5 +34,17 @@ xinput set-prop "$touchpad" "Synaptics Palm Dimensions" 5, 10
 # Tap time is in Tap durations and Tap time. 'Tap Time' messes with tapping, just set it to 1000!
 xinput set-prop "$touchpad" "Synaptics Tap Time" 200
 xinput set-prop "$touchpad" "Synaptics Tap Durations" 200 150 100
+
+xinput set-prop "$touchpad" "Synaptics Tap Durations" 200 150 100
+
+# Set speed
+xinput set-prop "$touchpad" "Device Accel Velocity Scaling" 20
+
+# Set touchpad area
+xinput set-prop "$touchpad" "Synaptics Area" 0 3000 0 2000
+
+# Disable touchpad while typing
+syndaemon -i 1 -K -d
+
 
 echo "Done!"
