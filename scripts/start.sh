6678d4b441f9d2104e8d06be7cd8d751384c9d51
Vid Vranjek <vidvranjek@gmail.com>
2018-11-15 23:09:41 +0100

    Themes update

--- scripts/start.sh
+++ scripts/start.sh
@@ -173,14 +173,23 @@ fi
 		      	[Yy]* ) echo "Installing theme"
 				#sudo add-apt-repository ppa:numix/ppa -y
 				sudo add-apt-repository ppa:system76/pop -y
+				sudo add-apt-repository ppa:noobslab/themes -y
 
 				sudo apt-get install materia-gtk-theme -y
 				sudo apt-get install pop-gtk-theme -y
+				sudo apt install system76-wallpapers -y
 
-				echo "Setting theme and icons"
-				gsettings set org.gnome.desktop.interface gtk-theme "Materia"
-				gsettings set org.gnome.desktop.wm.preferences theme "Materia"
+				echo "Setting theme and icons and wallpaper"
+				gsettings set org.gnome.desktop.interface gtk-theme "Materia-compact"
+				gsettings set org.gnome.desktop.wm.preferences theme "Materia-compact"
 				gsettings set org.gnome.desktop.interface icon-theme "Pop"
+				gsettings set org.gnome.desktop.background picture-uri
+'file:///usr/share/backgrounds/System76-Fractal_Mountains-by_Kate_Hazen_of_System76.png'
+				gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ background-color '#2e3436ff'
+				gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ launcher-opacity 0.8045977011494253
+				gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ panel-opacity 0.34980988593155893
+				gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ icon-size 32
+				
 				break;;
 		       	[Nn]* ) echo "NO"; break;;
 				* ) echo "Please answer yes or no.";;
