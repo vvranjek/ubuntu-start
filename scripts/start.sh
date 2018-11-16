a6dd6c183d206dc9310299695fdfd20328270a96
Vid Vranjek <vid.vranjek@gmail.com>
2018-11-16 10:53:44 +0100

    Fixed themes installation

--- scripts/start.sh
+++ scripts/start.sh
@@ -168,23 +168,24 @@ else
 fi
 
 	while true; do
-	read -p "Do you want to install and set Materia theme and Pop icons? (Y/N)?" answer
+	read -p "Do you want to install and set Unity, Materia theme and Pop icons? (Y/N)?" answer
 		case $answer in
 		      	[Yy]* ) echo "Installing theme"
 				#sudo add-apt-repository ppa:numix/ppa -y
 				sudo add-apt-repository ppa:system76/pop -y
 				sudo add-apt-repository ppa:noobslab/themes -y
 
+				sudo apt-get install ubuntu-unity-desktop -y
 				sudo apt-get install materia-gtk-theme -y
 				sudo apt-get install pop-gtk-theme -y
-				sudo apt install system76-wallpapers -y
+				sudo apt-get install pop-icon-theme -y
+				sudo apt-get install system76-wallpapers -y
 
 				echo "Setting theme and icons and wallpaper"
 				gsettings set org.gnome.desktop.interface gtk-theme "Materia-compact"
 				gsettings set org.gnome.desktop.wm.preferences theme "Materia-compact"
 				gsettings set org.gnome.desktop.interface icon-theme "Pop"
-				gsettings set org.gnome.desktop.background picture-uri
-'file:///usr/share/backgrounds/System76-Fractal_Mountains-by_Kate_Hazen_of_System76.png'
+				gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/System76-Fractal_Mountains-by_Kate_Hazen_of_System76.png'
 				gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ background-color '#2e3436ff'
 				gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ launcher-opacity 0.8045977011494253
 				gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ panel-opacity 0.34980988593155893
