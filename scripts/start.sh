7b6caf9ad116a09ca27433569fde6658146d4d1c
Vid Vranjek <vidvranjek@gmail.com>
2019-03-12 18:20:45 +0100

    Fixed installation of ubuntu-restricted-extras

--- scripts/start.sh
+++ scripts/start.sh
@@ -132,13 +132,17 @@ done
 while true; do
 	read -p "Do you want to install some apps (Y/N)?" answer
 	case $answer in
-	[Yy]* ) 	
+	[Yy]* ) 
+	sudo DEBIAN_FRONTEND=noninteractive apt-get -y install ubuntu-restricted-extras
+	sudo DEBIAN_FRONTEND=noninteractive apt-get -y install ubuntu-restricted-addons	
 	cat $FILENAME | while read -r line; do
 		# Check for commented lines
 		fs="$(echo $line | head -c 1)"
 
 		if [ "$fs" != "#" ]; then
 			sudo apt-get install $line -y
+		else
+			echo Ignoring commented package $line
 		fi
 			
 		indicator-multiload & disown
