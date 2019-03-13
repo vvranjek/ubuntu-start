71af4ae1d6da3c4a7c1701a9ce041bd01cd1e86b
Your Name <vidvranjek@gmail.com>
2019-03-13 10:32:47 +0100

    Added backintime

--- scripts/start.sh
+++ scripts/start.sh
@@ -112,11 +112,12 @@ while true; do
 	read -p "Do you want to add a bunch of repos (Y/N)?" answer
 	case $answer in
 	[Yy]* ) 
-		sudo add-apt-repository ppa:noobslab/apps   -y    #open-as-administrator 
-		sudo add-apt-repository ppa:nilarimogard/webupd8 -y #Prime indicator 
-		sudo add-apt-repository ppa:appgrid/stable -y      #Appgrid
-		sudo apt-add-repository ppa:rael-gc/scudcloud -y  # Slack
+		sudo add-apt-repository ppa:noobslab/apps   -y    		  #open-as-administrator 
+		sudo add-apt-repository ppa:nilarimogard/webupd8 -y 		  #Prime indicator 
+		sudo add-apt-repository ppa:appgrid/stable -y      		  #Appgrid
+		sudo apt-add-repository ppa:rael-gc/scudcloud -y  		  # Slack
 		sudo apt-add-repository ppa:freecad-maintainers/freecad-stable -y # FreeCAD
+		sudo add-apt-repository ppa:bit-team/stable -y 			  # Back in time 
 
 		echo "Removing unknow sources"
 		$SCRIPTS_DIR/remove-repos.sh
