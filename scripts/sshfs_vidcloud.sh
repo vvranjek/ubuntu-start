784530c98e77a4b06a71ee07bfde915d145723f3
Vid Vranjek <vidvranjek@gmail.com>
2019-08-28 08:33:01 +0200

    Fixed sshfs_vidcloud

--- scripts/sshfs_vidcloud.sh
+++ scripts/sshfs_vidcloud.sh
@@ -1,9 +1,15 @@
 
 #! /bin/bash
 
-ssh vid@vidcloud.myqnapcloud.com
-ssh-keygen
-ssh-copy-id -i ~/.ssh/id_rsa.pub vid@vidcloud.myqnapcloud.com
+#ssh vid@vidcloud.myqnapcloud.com
+#ssh-keygen
+#ssh-copy-id -i ~/.ssh/id_rsa.pub vid@vidcloud.myqnapcloud.com
 
-sudo sshfs -o allow_other vid@vidcloud.myqnapcloud.com:/share/CACHEDEV1_DATA/NAS/ /media/vid/NASsh
+MOUNT_POINT=/media/$USER/NAS2
+
+mkdir -p $MOUNT_POINT
+
+sudo sshfs -o reconnect,allow_other,IdentityFile=/home/vid/.ssh/id_rsa vid@vidcloud.myqnapcloud.com:/share/CACHEDEV1_DATA/NAS/ $MOUNT_POINT
+
+#sudo sshfs -o allow_other vid@vidcloud.myqnapcloud.com:/share/CACHEDEV1_DATA/NAS/ /media/vid/NASsh
 
