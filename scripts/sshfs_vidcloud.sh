cf02659b89b7e8a374d19de2b81f702de0266968
Vid Vranjek <vidvranjek@gmail.com>
2020-01-21 16:53:17 +0100

    Merge

--- scripts/sshfs_vidcloud.sh
+++ scripts/sshfs_vidcloud.sh
@@ -1,15 +1,16 @@
-
 #! /bin/bash
 
 #ssh vid@vidcloud.myqnapcloud.com
 #ssh-keygen
 #ssh-copy-id -i ~/.ssh/id_rsa.pub vid@vidcloud.myqnapcloud.com
 
-MOUNT_POINT=/media/$USER/NAS2
+MOUNT_SOURCE="/"
+MOUNT_TARGET=/media/$USER/NAS
 
-mkdir -p $MOUNT_POINT
 
-sudo sshfs -o reconnect,allow_other,IdentityFile=/home/vid/.ssh/id_rsa vid@vidcloud.myqnapcloud.com:/share/CACHEDEV1_DATA/NAS/ $MOUNT_POINT
+sudo mkdir -p $MOUNT_TARGET
+
+sudo sshfs -o reconnect,allow_other,IdentityFile=/home/vid/.ssh/id_rsa vid@vidcloud.myqnapcloud.com:$MOUNT_SOURCE $MOUNT_TARGET
 
 #sudo sshfs -o allow_other vid@vidcloud.myqnapcloud.com:/share/CACHEDEV1_DATA/NAS/ /media/vid/NASsh
 
