cc7e845cf6580b5cb5f7da733ca50c8555f02e56
Vid Vranjek <vid.vranjek@crea.com>
2020-02-03 11:23:48 +0100

    Improve bash-rc content

--- scripts/update_bashrc.sh
+++ scripts/update_bashrc.sh
@@ -4,43 +4,29 @@ SCRIPT=`realpath $0`
 SCRIPTS_DIR=`dirname $SCRIPT`
 
 
+ADD_LINE="source ~/bin/content/bashrc-content"
+
+
 echo
 
-if grep "Added by bashrc-add.sh" /home/$USER/.bashrc ; then
-	echo ".bashrc already contains modifications from this script."
+if [ -f /home/$USER/.bashrc ]; then
+    echo "Adding modifications to bashrcc"
+    if grep "$ADD_LINE" /home/$USER/.bashrc ; then
+        echo ".bashrc already contains modifications from this script."
+    else
+        while true; do
+            read -p 'Do you want to add modifications to .bashrc? (Y/N)?' answer
+                case $answer in
+                [Yy]* ) echo "Running bashrc script"
+                    echo "$ADD_LINE" >> /home/$USER/.bashrc
+                break;;
+                [Nn]* ) echo "NO"; break;;
+                * ) echo "Please answer yes or no.";;
+                esac
+        done
+    fi
 else
-	while true; do
-    	read -p "Do you want to add modifications to .bashrc? (Y/N)?" answer
-    		case $answer in
-        	[Yy]* ) echo "Running bashrc script"
-        	############################### Command ##################################
-			if [ -f /home/$USER/.bashrc ];
-            then
-                echo "File .bashrc exists. "
-
-                echo "Adding modifications to bashrcc"
-                if grep "Added by bashrc-add.sh" /home/$USER/.bashrc ; then
-                    echo ".bashrc already contains modifications from this script."
-                else
-                    while true; do
-                        read -p "Do you want to add modifications to .bashrc? (Y/N)?" answer
-                            case $answer in
-                            [Yy]* ) echo "Running bashrc script"
-                            $SCRIPTS_DIR/bashrc-add.sh /home/$USER/.bashrc
-                            break;;
-                            [Nn]* ) echo "NO"; break;;
-                            * ) echo "Please answer yes or no.";;
-                            esac
-                    done
-                fi
-            else
-                echo "File .bashrc does not exist."
-            fi
-            ##########################################################################
- 			break;;
-        	[Nn]* ) echo "NO"; break;;
-        	* ) echo "Please answer yes or no.";;
-    		esac
-	done
+    echo 'File .bashrc does not exist.'
 fi
+
 
--- scripts/update_zshrc.sh
+++ scripts/update_zshrc.sh
@@ -2,6 +2,8 @@
 
 SCRIPTS_DIR=$(find `pwd` -name "scripts")
 
+ADD_LINE="source ~/bin/content/bashrc-content"
+
 
 echo "This will update .zshrc"
 
@@ -10,14 +12,14 @@ then
    	echo "File .zshrc exists. "
 
 	echo "Adding modifications to zshrc"
-	if grep "Added by bashrc-add.sh" /home/$USER/.zshrc ; then
+	if grep "$ADD_LINE" /home/$USER/.zshrc ; then
 		echo ".zshrc already contains modifications from this script."
 	else
 		while true; do
 	    	read -p "Do you want to add modifications to .zshrc? (Y/N)?" answer
 	    		case $answer in
 			[Yy]* ) echo "Running zshrc script"
-				$SCRIPTS_DIR/bashrc-add.sh /home/$USER/.zshrc
+                echo "$ADD_LINE" >> /home/$USER/.zshrc
 	 			break;;
 			[Nn]* ) echo "NO"; break;;
 			* ) echo "Please answer yes or no.";;
