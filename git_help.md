d89785aad51bf202658dda0c70af7359812ae597
Vid Vranjek <vidvranjek@gmail.com>
2021-09-27 15:01:42 +0200

    Add mouse.py

--- git_help.md
+++ git_help.md
@@ -13,3 +13,8 @@ git remote add origin https://github.com/vvranjek/ubuntu-start.git
 git push -u origin master
 
 
+# To setup for new authentication on a cloned repo, do:
+git remote set-url origin https://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@github.com/vvranjek/REPO
+
+
+
---
+++ scripts/mouse.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+
+#from autopilot.input import Mouse
+import time
+import random
+import pyautogui
+import sys
+import datetime
+
+timeout = float(sys.argv[1])
+print("Timeout: %s" % timeout)
+ts = time.time()
+
+endtime = (ts + (timeout*60))
+
+print("Setting timer for %s minutes (%ss)." % (timeout, (timeout*60)))
+
+
+x = 100
+y = 100
+
+while True:
+    pyautogui.moveTo(random.randint(1920, 1920), random.randint(300, 800), duration=0.5)
+    pyautogui.click()
+
+    time.sleep(30)
+
+    if time.time() > endtime:
+        print("Exiting")
+        exit(0)
